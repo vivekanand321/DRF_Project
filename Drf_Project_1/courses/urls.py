d2163efe2169a6060987a003b436f23d28a74145
Vivekanand Pandey <pandeyvivek203@gmail.com>
2024-09-19 12:45:54 +0530

    added geneic view

--- Drf_Project_1/courses/urls.py
+++ Drf_Project_1/courses/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 app_name="courses"
+# router=routers.DefaultRouter()
+# router.register('/',views.CourseListCreateView.as_view())
 urlpatterns = [
-    path('', views.get_courses),
-    path('<int:pk>/', views.get_course),
+    # path('', views.get_courses),
+    # path('<int:pk>/', views.get_course),
+    path('', views.CourseListCreateView.as_view(),name='courses'),
 
 ]
